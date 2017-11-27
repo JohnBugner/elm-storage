7057daeda389383a2a1daa4f844728f31552080c
John Bugner <john.bugner@gmail.com>
2017-11-27 07:54:56 -0600

    Changed 'keys' and 'clear' from variables to functions.

--- src/Native/Local.js
+++ src/Native/Local.js
@@ -30,15 +30,19 @@ var _JohnBugner$elm_storage$Native_Local = function() {
         });
     }
 
-    var keys = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
-        var keyList = _elm_lang$core$Native_List.Nil;
+    // Ignore the _ parameter. I don't know how to make a scheduled function with 0 parameters,
+    // but I do know how to make one with 1, so the 1 parameter is just a placeholder.
+    function keys(_) {
+        return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
+            var keyList = _elm_lang$core$Native_List.Nil;
 
-        for (var i = window.localStorage.length; i > 0 ; --i) {
-            keyList = _elm_lang$core$Native_List.Cons(window.localStorage.key(i - 1), keyList);
-        }
+            for (var i = window.localStorage.length; i > 0 ; --i) {
+                keyList = _elm_lang$core$Native_List.Cons(window.localStorage.key(i - 1), keyList);
+            }
 
-        callback(_elm_lang$core$Native_Scheduler.succeed(keyList));
-    });
+            callback(_elm_lang$core$Native_Scheduler.succeed(keyList));
+        });
+    }
 
     function remove(key) {
         return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
@@ -48,11 +52,13 @@ var _JohnBugner$elm_storage$Native_Local = function() {
         });
     }
 
-    var clear = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
-        window.localStorage.clear();
+    function clear(_) {
+        return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
+            window.localStorage.clear();
 
-        callback(_elm_lang$core$Native_Scheduler.succeed(_elm_lang$core$Native_Utils.Tuple0));
-    });
+            callback(_elm_lang$core$Native_Scheduler.succeed(_elm_lang$core$Native_Utils.Tuple0));
+        });
+    }
 
     return {
         rawSet : F2(rawSet),
